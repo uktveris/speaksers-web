81d47354921d616feb74a19e4e8cf25b74455970
uktveris <marius.uktveris@gmail.com>
2025-10-26 15:01:34 +0100

    added management middleware

---
+++ app/middleware.ts
@@ -0,0 +1,73 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const hostname = request.headers.get("host") || "";
+
+  let env: "dev" | "preview" | "prod";
+  if (hostname.includes("dev.")) {
+    env = "dev";
+  } else if (hostname.includes("preview.")) {
+    env = "preview";
+  } else {
+    env = "prod";
+  }
+
+  if (
+    request.nextUrl.pathname === "/.well-known/apple-app-site-association" ||
+    request.nextUrl.pathname === "/apple-app-site-association"
+  ) {
+    const aasa = getAASA(env);
+
+    return new NextResponse(JSON.stringify(aasa), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "public, max-age=3600",
+      },
+    });
+  }
+
+  if (request.nextUrl.pathname === "/.well-known/assetlinks.json") {
+    const assetlinks = getAssetLinks(env);
+
+    return new NextResponse(JSON.stringify(assetlinks), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "public, max-age=3600",
+      },
+    });
+  }
+
+  return NextResponse.next();
+}
+
+function getAASA(env: "dev" | "preview" | "prod") {
+  const suffix = env === "prod" ? "" : `.${env}`;
+
+  return {
+    applinks: {
+      apps: [],
+      details: [{ appID: `MJC38BUU22.com.speaksersedu.speaksers${suffix}`, paths: ["*"] }],
+    },
+  };
+}
+
+function getAssetLinks(env: "dev" | "preview" | "prod") {
+  const suffix = env === "prod" ? "" : `.${env}`;
+
+  return [
+    {
+      relation: ["delegate_permissions/common.handle_all_urls"],
+      target: {
+        namespace: "android_app",
+        package_name: `com.speaksersedu.speaksers${suffix}`,
+        sha256_cert_fingerprints: [process.env[`SHA256_FINGERPRINT_${env.toUpperCase()}`] || ""],
+      },
+    },
+  ];
+}
+
+export const config = {
+  mather: ["/.well-known/apple-app-site-association", "/apple-app-site-association", "/.well-known/assetlinks.json"],
+};
